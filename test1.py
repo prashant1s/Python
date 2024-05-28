09c5f5ad9b620a11b8535b75594bdb9edccdaf30
Prashant singh <prashantsingh20034@gmail.com>
2024-05-29 01:31:46 +0530

    first commit

---
+++ test1.py
@@ -0,0 +1,14 @@
+print("helloe world!")
+print("helloe world!")
+print("helloe wobnxfxnnrld!")
+a=[1,2,3,4,5]
+#print (a)
+a[0]= 12
+#print (a)
+
+b=set()
+b.add(4)
+b.add(7)
+b.add (9)
+b.add((4,5,6))
+print(b)
